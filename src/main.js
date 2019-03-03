caa845dfb597171fbe60289476da8b079830084b
Jinghy <857885610@qq.com>
2019-03-03 18:03:58 +0800

    bootstrap文件提到static目录

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import './assets/plugin/bootstrap/css/bootstrap.min.css'
+// import './assets/plugin/bootstrap/css/bootstrap.min.css'
 import router from './router'
 
 Vue.config.productionTip = false
